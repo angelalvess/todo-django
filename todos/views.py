0650aeda050a188647243d10d4f9011a906adc6d
angelalves <angeltrashangel@gmail.com>
2024-08-26 19:32:01 -0300

    feat: Add Todo update and delete functionality
    This commit adds the necessary code changes to implement the update and delete functionality for Todo items. It includes the addition of `TodoUpdateView` and `TodoDeleteView` in the `todos/views.py` file, as well as corresponding URL patterns in the `setup/urls.py` file. The templates for the Todo form and Todo list have also been updated to include the appropriate links for editing and deleting Todo items.

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Todo
 from django.urls import reverse_lazy
 
@@ -13,4 +13,15 @@ class TodoListView(ListView):
 class TodoCreateView(CreateView):
     model = Todo
     fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoDeleteView(DeleteView):
+    model = Todo
     success_url = reverse_lazy("todo_list")
