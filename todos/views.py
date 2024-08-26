47aca211bed02dcf6d78f8b02749aaee09082704
angelalves <angeltrashangel@gmail.com>
2024-08-26 16:02:15 -0300

    chore: Update Django settings module environment variables

--- todos/views.py
+++ todos/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Todo
 
 # Create your views here.
 
 
-def home(request):
-    return render(request, "todos/base.html")
+def todo_list(request):
+    todos = Todo.objects.all()
+    return render(request, "todos/todo_list.html", {"todos": todos})
